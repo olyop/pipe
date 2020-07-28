0405739e9b89eb9eda5a007405a0edfbce308f73
Oliver <oliver.plummer@outlook.com>
2020-07-28 15:30:44 +1000

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
 type UnknownFunction = (...params: unknown[]) => unknown
 
-export function pipe<A extends unknown[], B>(
+function pipe<A extends unknown[], B>(
 	ab: (this: void, ...a: A) => B,
 ): (...args: A) => B
 
-export function pipe<A extends unknown[], B, C>(
+function pipe<A extends unknown[], B, C>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 ): (...args: A) => C
 
-export function pipe<A extends unknown[], B, C, D>(
+function pipe<A extends unknown[], B, C, D>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
 ): (...args: A) => D
 
-export function pipe<A extends unknown[], B, C, D, E>(
+function pipe<A extends unknown[], B, C, D, E>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
 	de: (this: void, d: D) => E,
 ): (...args: A) => E
 
-export function pipe<A extends unknown[], B, C, D, E, F>(
+function pipe<A extends unknown[], B, C, D, E, F>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
 	de: (this: void, d: D) => E,
 	ef: (this: void, e: E) => F,
 ): (...args: A) => F
-export function pipe<A extends unknown[], B, C, D, E, F, G>(
+
+function pipe<A extends unknown[], B, C, D, E, F, G>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
@@ -38,7 +39,7 @@ export function pipe<A extends unknown[], B, C, D, E, F, G>(
 	fg: (this: void, f: F) => G,
 ): (...args: A) => G
 
-export function pipe<A extends unknown[], B, C, D, E, F, G, H>(
+function pipe<A extends unknown[], B, C, D, E, F, G, H>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
@@ -48,7 +49,7 @@ export function pipe<A extends unknown[], B, C, D, E, F, G, H>(
 	gh: (this: void, g: G) => H,
 ): (...args: A) => H
 
-export function pipe<A extends unknown[], B, C, D, E, F, G, H, I>(
+function pipe<A extends unknown[], B, C, D, E, F, G, H, I>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
@@ -59,7 +60,7 @@ export function pipe<A extends unknown[], B, C, D, E, F, G, H, I>(
 	hi: (this: void, h: H) => I,
 ): (...args: A) => I
 
-export function pipe<A extends unknown[], B, C, D, E, F, G, H, I, J>(
+function pipe<A extends unknown[], B, C, D, E, F, G, H, I, J>(
 	ab: (this: void, ...a: A) => B,
 	bc: (this: void, b: B) => C,
 	cd: (this: void, c: C) => D,
@@ -71,10 +72,12 @@ export function pipe<A extends unknown[], B, C, D, E, F, G, H, I, J>(
 	ij: (this: void, i: I) => J,
 ): (...args: A) => J
 
-export default function pipe(...fns: UnknownFunction[]): UnknownFunction {
+function pipe(...fns: UnknownFunction[]): UnknownFunction {
 	return (...initialParams: unknown[]): unknown =>
 		fns.reduce<unknown>((value, fn, index) => {
 			const params = index === 0 ? (value as unknown[]) : [value]
 			return fn(...params)
 		}, initialParams)
 }
+
+export default pipe
