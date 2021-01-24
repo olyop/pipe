8fd2e2b9f78993f6a03f65ac0b840f49676f7fc8
Oliver <oliver.plummer@outlook.com>
2021-01-25 10:14:43 +1100

    Update

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function pipe(...fns) {
-    return (...initialParams) => fns.reduce((value, fn, index) => {
-        const params = index === 0 ? value : [value];
-        return fn(...params);
-    }, initialParams);
-}
-exports.default = pipe;
